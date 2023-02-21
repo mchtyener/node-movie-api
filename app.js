ba13c7dc3f372fd3b92176c48b7b33e17065694e
mucahit <mucahit@mucahit-Air.home>
2023-02-21 11:11:32 +0300

    update and detail services have been created

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/movie', movie);
+app.use('/api/movies', movie);
 
 app.use(function (req, res, next) {
     next(createError(404));
--- routes/movie.js
+++ routes/movie.js
@@ -7,10 +7,52 @@ router.post('/', function (req, res, next) {
     const movie = new Movie(req.body)
     const promise = movie.save();
     promise.then((data) => {
-        res.json(data)
+        res.json({
+            data: data,
+            status: true,
+        })
     }).catch((err) => {
-        res.json(err)
+        res.json({message: err.message})
     })
 });
+
+//movie all list
+router.get('/', function (req, res, next) {
+    const promise = Movie.find();
+    promise.then((data) => {
+        res.json({
+            data: data,
+            status: true,
+        })
+    }).catch((err) => {
+        res.json({message: err.message})
+    })
+});
+
+// movie detail
+router.get('/:movie_id', function (req, res, next) {
+    const promise = Movie.findById(req.params.movie_id);
+    promise.then((data) => {
+        res.json({
+            data: data,
+            status: true,
+        })
+    }).catch((err) => {
+        res.json({message: err.message})
+    })
+});
+
+router.put('/:movie_id', function (req, res, next) {
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true}); //new dönüşten sonran en güncel halini veriyor
+    promise.then((data) => {
+        res.json({
+            data: data,
+            status: true,
+        })
+    }).catch((err) => {
+        res.json({message: err.message})
+    })
+});
+
 
 module.exports = router;

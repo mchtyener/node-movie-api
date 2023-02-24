0c131c8d741b052ff79598f8d5d7be99bef6da86
mucahit <mucahit@PC192.168.1.2.station>
2023-02-24 11:39:50 +0300

    The user creation service has been created. User password is hidden with Crypto.js. Env file added.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -30,14 +31,14 @@ app.use(function (req, res, next) {
     next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.json({ error: { message: err.message, code: err.code } });
+    res.json({error: {message: err.message, code: err.code}});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const CryptoJS = require("crypto-js");
+const User = require("../models/User")
+const status = require("http-status-codes");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+
+router.post('/register', function (req, res, next) {
+
+    const {username, password} = req.body
+    const user = new User({
+        username: username,
+        password: CryptoJS.AES.encrypt(password, process.env.CRYPTO_KEY).toString()
+    })
+    const promise = user.save();
+
+    promise.then((data) => {
+        res.json({
+            data: data,
+            status: status.OK,
+        })
+    }).catch((err) => {
+        res.json({message: err.message})
+    })
+});
+
 
 module.exports = router;
